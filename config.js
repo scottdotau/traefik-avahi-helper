b6788baf424930700f17634eed8bd9f1b6022de0
Scott G <479915+scottdotau@users.noreply.github.com>
2023-05-17 14:59:24 +1000

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+export const labelRegExp = /traefik\.http\.routers\.(.*)\.rule/
+export const hostRegExp = /Host\(\s*`(.*?\.local)`\s*,*\s*\)/gi
+export const domainRegExp = /`(?<domain>[^`]*?\.local)`/g
