70be02c435d040ceaaa58ab0455f88a07d1500a3
Scott <scott.au@pm.me>
2023-05-17 21:20:24 +1000

    modernize that shiz

--- index.js
+++ index.js
@@ -4,23 +4,36 @@ import nodemon from 'nodemon';
 import { labelRegExp, hostRegExp, domainRegExp, matchDomainCnames } from './lib.js';
 
 const docker = new Docker({socketPath: "/var/run/docker.sock"});
-let cnames = [];
+const InSwarm = !!(await docker.info()).Swarm;
+let cnames = []; 
 
-docker.listContainers()
+const mapTarget = t => {
+  const IsSwarmService = !!t.Spec;
+  const target = IsSwarmService ? t.Spec : t;
+  const name = IsSwarmService ? target.Name : target.Names[0].substring(1)
+  return {
+    IsSwarmService,
+    TargetName: name,
+    ...target, 
+    _original: IsSwarmService ? t : null
+  } 
+}
+
+Promise.all([docker.listContainers(), InSwarm ? docker.listServices() : Promise.resolve([])])
 .then( list => {
-  for (var i=0; i<list.length; i++) {
-    var cont = list[i]
-    if (cont.Labels["traefik.enable"] === "true") {
-      var name = cont.Names[0].substring(1)
-      var keys = Object.keys(cont.Labels)
-      keys.forEach(key =>{
-        if (re.test(key) && hostRegExp.test(cont.Labels[key])) {
-          hostRegExp.lastIndex=0
-          cnames = cnames.concat(matchDomainCnames(cont.Labels[key]))
+  const targets = list.flat()
+    .map(t => mapTarget(t)) 
+    .filter(t => ('traefik.enable' in t.Labels))
+  
+  for (var target of targets) {
+      Object.keys(target.Labels).forEach(label => {
+        if (labelRegExp.test(label) && hostRegExp.test(target.Labels[label])) {
+          hostRegExp.lastIndex = 0;
+          cnames = cnames.concat(matchDomainCnames(target.Labels[label]))
         }
       })
-    }
   }
+
   console.log(cnames)
   fs.writeFile("cnames",cnames.join('\n'), 'utf8', err => {}) 
 
@@ -39,17 +52,18 @@ docker.listContainers()
   })
 })
 .then(() => {
-  docker.getEvents({filters: { event: ["start", "stop"]}})
+  const startEvents = ['start', 'create', 'update'];
+  const stopEvents = ['stop', 'remove'];
+  docker.getEvents({filters: { event: [ ...startEvents, ...stopEvents ]}})
   .then( events => {
     events.setEncoding('utf8');
-    events.on('data',ev => {
+    events.on('data', ev => {
       var eventJSON = JSON.parse(ev)
-      if(!['start', 'stop'].includes(eventJSON.status)){
+      if(![ ...startEvents, ...stopEvents  ].includes(eventJSON.status)){
         return;
       }
-
-      var keys = Object.keys(eventJSON.Actor.Attributes)
-      keys.forEach(key => {
+      
+      Object.keys(eventJSON.Actor.Attributes).forEach(key => {
         if (!labelRegExp.test(key)) {
           return;
         }
@@ -59,10 +73,10 @@ docker.listContainers()
           return;
         }
 
-        if (eventJSON.status === 'start') {
+        if (startEvents.contains(eventJSON.status)) {
           cnames = cnames.concat(hosts)
           console.log('Adding', hosts);
-        } else if (eventJSON.status === 'stop') {
+        } else if (stopEvents.contains(eventJSON.status)) {
           cnames = cnames.filter(host => !hosts.includes(host));
           console.log('Removing', hosts);
         }
--- lib.js
+++ lib.js
@@ -1,6 +1,6 @@
 export const labelRegExp = /traefik\.http\.routers\.(.*)\.rule/
-export const hostRegExp = /Host\(\s*`(.*?\.local)`\s*,*\s*\)/gi
-export const domainRegExp = /`(?<domain>[^`]*?\.local)`/g
+export const hostRegExp = /Host\(\s*`(.*?\.(local|lan))`\s*,*\s*\)/gi
+export const domainRegExp = /`(?<domain>[^`]*?\.(local|lan))`/g
 export const matchDomainCnames = function (domainString) {
   return [...domainString.matchAll(domainRegExp)].map(match => match.groups.domain)
 }
